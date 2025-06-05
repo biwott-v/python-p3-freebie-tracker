fa26ffdbc2e537c720a09306ccb894bb4b6850c9
biwott-v <victor.biwott@student.moringaschool.com>
2025-06-05 13:01:47 +0300

    adds methods to poppulate thadatabase

--- lib/seed.py
+++ lib/seed.py
@@ -15,3 +15,14 @@ session.query(Freebie).delete()
 session.query(Company).delete()
 session.query(Dev).delete()
 session.commit()
+
+company1 = Company(name="safaricom", founding_year=1997)
+company2 = Company(name="tangazolety",founding_year=2004)
+
+
+dev1= Dev(name="john")
+dev2 =Dev(name="elliot")
+
+freebie1 = Freebie(item_name="Tshirt", value=3,dev=dev1,company=company1)
+freebie2 = Freebie(item_name="water bottle", value=4,dev=dev2,company=company2)
+
