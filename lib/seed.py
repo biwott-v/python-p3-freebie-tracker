6e8a45853a5ff4b0e39c2e6ff1a83ecd8e8de6df
biwott-v <victor.biwott@student.moringaschool.com>
2025-06-05 12:56:17 +0300

    imports sqlalchemy and classes

--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from models import Company, Dev, Freebie, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///freebies.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
